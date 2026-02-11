6a38b1f785f5deadc69500b485886517e0d1cbe4
MuRALLab <murallab26@gmail.com>
2026-02-11 10:11:00 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       .scrollIntoView({ behavior: "smooth" });
   });
 });
+
+const slides = document.querySelectorAll(".slide");
+let current = 0;
+
+function nextSlide() {
+  slides[current].classList.remove("active");
+  current = (current + 1) % slides.length;
+  slides[current].classList.add("active");
+}
+
+setInterval(nextSlide, 5000);
